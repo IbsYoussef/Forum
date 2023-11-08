0e56b5a0637b8fa69088146001091e257311a894
iyoussef <ibraheemyoussefiy@gmail.com>
2023-11-08 16:45:05 +0000

    Added DB/Organised Files

---
+++ sql.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE USERS (username TEXT NOT NULL PRIMARY KEY, 
+-- password NOT NULL, email NOT NULL); 
+
+-- INSERT INTO USERS (username, password, email)
+-- VALUES("james", "jamesmypassword", "jamesMicheal@gmail.com"); 
+
+-- SELECT * FROM USERS;
+
+-- SELECT email FROM USERS
+-- WHERE username = "joe";
+
+-- SELECT password FROM USERS
+-- WHERE username IN ("joe", "mike");
+
+-- UPDATE USERS SET email="newEmail@gmail.com"
+-- WHERE username = "mike";
+
+-- DELETE FROM USERS
+-- WHERE username = "joe";
+
+-- SELECT username, password, email FROM USERS
