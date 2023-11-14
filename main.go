c0213ad90898198fad77bb727b48ebd08721fe77
iyoussef <ibraheemyoussefiy@gmail.com>
2023-11-14 19:17:10 +0000

    Added Email Existence Check (Must clean up)

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"html/template"
+	"forum/packages/methods"
 	"log"
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
-	"golang.org/x/crypto/bcrypt"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(tpl.ParseGlob("templates/*.html"))
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/register", RegisterPageHandler)
-	http.HandleFunc("/message", RegisterUserInfo)
+	http.HandleFunc("/", methods.IndexHandler)
+	http.HandleFunc("/register", methods.RegisterPageHandler)
+	http.HandleFunc("/message", methods.RegisterUserInfo)
 
 	assets := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
@@ -31,55 +23,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Printf("Error code: %s", err.Error())
 	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Error 404 Page not found", 404)
-	} else {
-		tpl.ExecuteTemplate(w, "index.html", nil)
-	}
-}
-
-const INSERT = "INSERT INTO USERS (username, password, email) VALUES (?,?,?)"
-
-func RegisterPageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/register" {
-		http.Error(w, "Error 404 Page not found", 404)
-	} else {
-		tpl.ExecuteTemplate(w, "register.html", nil)
-	}
-}
-
-func RegisterUserInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Error Page 404 not found", 404)
-		return
-	}
-
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	hashedPassword, Herr := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if Herr != nil {
-		fmt.Printf("Error Hashing Password: Error Code %v", Herr)
-	}
-
-	password = string(hashedPassword)
-	email := r.FormValue("email")
-
-	db, err := sql.Open("sqlite3", "userdb.db")
-	if err != nil {
-		http.Error(w, "Error opening the database", http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
-
-	_, err = db.Exec(INSERT, username, password, email)
-	if err != nil {
-		http.Error(w, "Error inserting data into the database", http.StatusInternalServerError)
-		return
-	}
-
-	tpl.ExecuteTemplate(w, "message.html", nil)
 }
--- methods/register_user.go
+++ methods/register_user.go
@@ -1,7 +1,86 @@
 package methods
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var tpl *template.Template
+var db *sql.DB
+
+func init() {
+	tpl = template.Must(tpl.ParseGlob("templates/*.html"))
+}
+
+const INSERT = "INSERT INTO USERS (username, password, email) VALUES (?,?,?)"
+const FIND = "SELECT COUNT(*) FROM users WHERE email = ?"
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Error 404 Page not found", 404)
+	} else {
+		tpl.ExecuteTemplate(w, "index.html", nil)
+	}
+}
+
+func RegisterPageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/register" {
+		http.Error(w, "Error 404 Page not found", 404)
+	} else {
+		tpl.ExecuteTemplate(w, "register.html", nil)
+	}
+}
+
+func RegisterUserInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Error Page 404 not found", 404)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Error parsing form data", http.StatusInternalServerError)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+
+	db, err := sql.Open("sqlite3", "userdb.db")
+	if err != nil {
+		http.Error(w, "Error opening the database", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	var count int
+	err = db.QueryRow(FIND, email).Scan(&count)
+	if err != nil {
+		http.Error(w, "Error checking email existence in the database", http.StatusInternalServerError)
+		return
+	}
+
+	if count > 0 {
+		http.Error(w, "Email is already taken", http.StatusBadRequest)
+		return
+	} else {
+		hashedPassword, Herr := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if Herr != nil {
+			fmt.Printf("Error Hashing Password: Error Code %v", Herr)
+		}
+		password = string(hashedPassword)
+
+		_, err = db.Exec(INSERT, username, password, email)
+		if err != nil {
+			http.Error(w, "Error inserting data into the database", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	tpl.ExecuteTemplate(w, "message.html", nil)
+}
