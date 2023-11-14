b578d1de0b377f7c9db78dc1ce31082915f961e1
iyoussef <ibraheemyoussefiy@gmail.com>
2023-11-14 12:53:35 +0000

    Register Functions Seperated

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"forum/packages/methods"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,8 +18,8 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/register", registerPageHandler)
-	http.HandleFunc("/message", getUserInfo)
+	http.HandleFunc("/register", methods.RegisterPageHandler)
+	http.HandleFunc("/message", methods.RegisterUserInfo)
 
 	assets := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
@@ -38,38 +38,4 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		tpl.ExecuteTemplate(w, "index.html", nil)
 	}
-}
-
-func registerPageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/register" {
-		http.Error(w, "Error 404 Page not found", 404)
-	} else {
-		tpl.ExecuteTemplate(w, "register.html", nil)
-	}
-}
-
-func getUserInfo(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Error Page 404 not found", 404)
-		return
-	}
-
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-	email := r.FormValue("email")
-
-	db, err := sql.Open("sqlite3", "userdb.db")
-	if err != nil {
-		http.Error(w, "Error opening the database", http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
-
-	_, err = db.Exec("INSERT INTO USERS (username, password, email) VALUES (?,?,?)", username, password, email)
-	if err != nil {
-		http.Error(w, "Error inserting data into the database", http.StatusInternalServerError)
-		return
-	}
-
-	tpl.ExecuteTemplate(w, "message.html", nil)
 }
