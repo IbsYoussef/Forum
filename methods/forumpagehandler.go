ba0e68fe1be12f5cc2423ec68394b3bdf04a81a4
iyoussef <ibraheemyoussefiy@gmail.com>
2023-11-21 16:55:34 +0000

    slight change to forum handler

--- methods/forumpagehandler.go
+++ methods/forumpagehandler.go
@@ -6,6 +6,6 @@ func ForumPageHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/mainpage" {
 		http.Error(w, "Error 404 Page not found", 404)
 	} else {
-		Tpl.ExecuteTemplate(w, "index.html", nil)
+		Tpl.ExecuteTemplate(w, "forumpage.html", nil)
 	}
 }
