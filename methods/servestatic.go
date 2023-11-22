6049d64881dc366daebc7b69ce9f19bc5433a451
tmachado <rosadelonfamily@gmail.com>
2023-11-22 10:47:32 +0000

    update to serve images

--- methods/servestatic.go
+++ methods/servestatic.go
@@ -4,5 +4,7 @@ import "net/http"
 
 func ServeStatic() {
 	assets := http.FileServer(http.Dir("assets"))
+	static := http.FileServer(http.Dir("static"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+	http.Handle("/static/", http.StripPrefix("/static/", static))
 }
